9af969f23ee7f5b2991d53f58794891760fede94
kashyapnathan <43067072+kashyapnathan@users.noreply.github.com>
2018-10-01 08:32:39 -0400

    Add files via upload
    10/01/18

---
+++ src/OperationsPractice.java
@@ -0,0 +1,20 @@
+
+public class OperationsPractice {
+
+	public static void main(String[] args) 
+	{
+		int number; //Data type then variable name
+		number = 20;
+		System.out.println(number);
+		number += 5;
+		number /= 2;
+		System.out.println(number);
+		number *= 4;
+		System.out.println(number);
+		number -= 6;
+		System.out.println(number);
+		number %= 2;
+		System.out.println(number);
+	}
+
+}
